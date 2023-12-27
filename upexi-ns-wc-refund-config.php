9c693a619a9a760f571527e83801834ddb90c21c
tylly <jamestylladams@gmail.com>
2023-12-27 21:23:43 +0100

    plugin confic init

---
+++ upexi-ns-wc-refund-config.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: Upexi WooCommerce Refund Integration for NetSuite
+ * Plugin URI: https://github.com/tylly/upexi-ns-wc-refund-intergration
+ * Description: Develop an endpoint for a webhook that automatically triggers when a refund is processed. This integration aims to streamline the workflow by ensuring real-time synchronization between our MoonWlkr (BE / WC) platform and NetSuite.
+ * Author: Upexi Inc.
+ * Version: 1.0
+ */
